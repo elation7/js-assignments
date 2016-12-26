a7611e569f05537b3812b09337cd411eb6b2f31d
elation <nadinpeach@gmail.com>
2016-12-27 00:29:38 +0300

    Corrected 3 task

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -128,8 +128,8 @@ function angleBetweenClockHands(date) {
     }
     var angleMinutes = 6 * minutes;
     var angledelta = Math.abs(angleHours - angleMinutes);
-    var angleInRad = Math.PI * angledelta / 360;
-    return angleInRad;
+    var angleInRad = Math.PI / 180 * angledelta;
+    return  angledelta;
 }
 
 
